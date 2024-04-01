42141cddd3a8a0aa891991a7402982dfba8e761e
Vincent Revard <Vincent.Revard@gmail.com>
2024-04-01 16:38:51 -0700

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -3,25 +3,91 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from models import db, Earthquake
+from models import Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
+    body = {"message": "Flask SQLAlchemy Lab 1"}
     return make_response(body, 200)
 
+
 # Add views here
+@app.route("/earthquakes/<int:id>", methods=["GET"])
+def view_by_id(id):
+    # if earthquake := Earthquake.query.filter_by(id=id).first():
+    if earthquake := db.session.get(Earthquake, id):
+        return earthquake.as_dict(), 200
+    else:
+        return {"message": f"Earthquake {id} not found."}, 404
+
+#! Not using to_dict or as_dict method - SQLAlchemy 1.4 version
+# @app.route("/earthquakes/magnitude/<float:magnitude>")
+# def view_by_min_mag(magnitude):
+#     quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+#     quake_list = [
+#         {
+#             "id": quake.id,
+#             "location": quake.location,
+#             "magnitude": quake.magnitude,
+#             "year": quake.year,
+#         }
+#         for quake in quakes
+#     ]
+#     count = len(quake_list)
+
+#     return {"count": count, "quakes": quake_list}, 200
+
+#! using to_dict or as_dict method - SQLAlchemy 1.4 version
+# @app.route("/earthquakes/magnitude/<float:magnitude>")
+# def view_by_min_mag(magnitude):
+#     quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude)
+#     quake_list = [quake.as_dict() for quake in quakes]
+
+#     count = len(quake_list)
+
+#     return {"count": count, "quakes": quake_list}, 200
+
+#! Session + SQLAlchemy 1.4 version
+# @app.route("/earthquakes/magnitude/<float:magnitude>")
+# def view_by_min_mag(magnitude):
+#     with Session(db.engine) as session:
+#         quakes = (
+#             session.query(Earthquake).filter(Earthquake.magnitude >= magnitude)
+#         )
+#         quake_list = [quake.as_dict() for quake in quakes]
+#         count = len(quake_list)
+
+#     return {"count": count, "quakes": quake_list}, 200
+
+# if __name__ == "__main__":
+#     app.run(port=5555, debug=True)
 
 
-if __name__ == '__main__':
+#! Sessions + SQLAlchemy 2.0 version
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def view_by_min_mag(magnitude):
+    with Session(db.engine) as session:
+        stmt = select(Earthquake).where(Earthquake.magnitude >= magnitude)
+        result = session.execute(stmt)
+        quakes = result.scalars().all()
+        quake_list = [quake.as_dict() for quake in quakes]
+        count = len(quake_list)
+
+    return {"count": count, "quakes": quake_list}, 200
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,37 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"""
+            <Earthquake # {self.id},
+            {self.magnitude},
+            {self.location},
+            {self.year}>
+        """
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "magnitude": self.magnitude,
+            "location": self.location,
+            "year": self.year,
+        }
+
+# class Planet(db.Model):
+#     __tablename__ = "planets"
+
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(50), nullable=False)
+
+#     def as_dict(self):
+#         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    
+    #!add one foreign ID, earthquake is the child, planet is the parent
